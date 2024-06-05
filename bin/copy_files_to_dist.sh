d4c3e7c2b1b8e5f139d69902d3a0ac23b0f8913d
ntakahashi016 <ntakahashi016@gmail.com>
2024-06-05 22:36:05 +0900

    add mkdir

--- bin/copy_files_to_dist.sh
+++ bin/copy_files_to_dist.sh
@@ -6,6 +6,7 @@ SOURCE_PATH="$ROOT_PATH/documents"
 DIST_PATH="$ROOT_PATH/dist"
 
 rm -rf $DIST_PATH
+mkdir $DIST_PATH
 
 cp $SOURCE_PATH/doc/openapi.yml $DIST_PATH
 cp $ROOT_PATH/swagger/* $DIST_PATH

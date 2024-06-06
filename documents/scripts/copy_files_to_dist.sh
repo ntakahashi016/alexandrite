730575425bdbffa6bfdcdf7cee51249015da5fed
ntakahashi016 <ntakahashi016@gmail.com>
2024-06-06 22:58:36 +0900

    change file location

--- documents/scripts/copy_files_to_dist.sh
+++ documents/scripts/copy_files_to_dist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 BIN_PATH="$(dirname -- ${0})"
-ROOT_PATH="$BIN_PATH/.."
+ROOT_PATH="$BIN_PATH/../../../"
 SOURCE_PATH="$ROOT_PATH/documents"
 DIST_PATH="$ROOT_PATH/dist"
 
 rm -rf $DIST_PATH
 mkdir $DIST_PATH
 
-cp $SOURCE_PATH/doc/openapi.yml $DIST_PATH
-cp $ROOT_PATH/swagger/* $DIST_PATH
+cp $SOURCE_PATH/api/openapi.yml $DIST_PATH
+cp $SOURCE_PATH/swagger/* $DIST_PATH

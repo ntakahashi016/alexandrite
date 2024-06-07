3571f55aa896e29c44ef573acf9cc0c0bcf0dfb4
ntakahashi016 <ntakahashi016@gmail.com>
2024-06-07 23:48:07 +0900

    added comments

--- app/client/lib/main.dart
+++ app/client/lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
+// 1. エントリーポイントのmain関数
 void main() {
+  // 2. MyAppを呼び出す
   runApp(const MyApp());
 }
 
+// MyAppのクラス
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    // 3. タイトルとテーマを設定する。画面の本体はMyHomePageで作る。
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -29,6 +33,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  // 5. カウンタが押された時のメソッド
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -37,7 +42,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // 4. MyHomePageの画面を構築する部分
     return Scaffold(
+      // 画面上部のタイトル部分
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
@@ -46,9 +53,11 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // 画面の中央に表示されるテキスト
             const Text(
               'You have pushed the button this many times:',
             ),
+            // テキストの下に表示されるカウンタ値
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -56,6 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      // 右下の「+」ボタンに対応するフローティングアクションボタン
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',

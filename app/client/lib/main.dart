8479e14c19cd8093d56bfdcd4351a2b596678223
ntakahashi016 <ntakahashi016@gmail.com>
2024-06-08 01:41:22 +0900

    add main menu grid

--- app/client/lib/main.dart
+++ app/client/lib/main.dart
@@ -31,46 +31,43 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  // 5. カウンタが押された時のメソッド
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+  List<String> mainMenu = ['貸出','返却','検索','履歴'];
   @override
   Widget build(BuildContext context) {
-    // 4. MyHomePageの画面を構築する部分
     return Scaffold(
-      // 画面上部のタイトル部分
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Row(children: const [
+          Icon(Icons.import_contacts),
+          Text("おうち図書館システム"),
+        ]),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // 画面の中央に表示されるテキスト
-            const Text(
-              'You have pushed the button this many times:',
+      body: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+        childAspectRatio: 1.5,
+        shrinkWrap: true,
+        children: List.generate(4, (index) {
+          return Container(
+            padding: const EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.green,
             ),
-            // テキストの下に表示されるカウンタ値
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            child: GridTile(
+              child: Icon(Icons.book),
+              footer: Center(
+                child: Text(
+                  '${mainMenu[index]}',
+                ),
+              )
             ),
-          ],
-        ),
+          );
+        }),
       ),
-      // 右下の「+」ボタンに対応するフローティングアクションボタン
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+        onPressed: () => {print("押したね？")}, child: const Icon(Icons.timer)),
+      drawer: const Drawer(child: Center(child: Text("Drawer"))),
     );
   }
 }

ef8ab60ae14c134906ea21a80c80e8fe41cdaaeb
ntakahashi016 <ntakahashi016@gmail.com>
2024-06-15 22:49:19 +0900

    [added] Implement AdminScreen

---
+++ app/client/lib/admin_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import './main_appbar.dart';
+import './main_drawer.dart';
+import './menu_item.dart';
+import './wip_screen.dart';
+
+class AdminScreen extends StatelessWidget {
+  const AdminScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const AdminScreenApp(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class AdminScreenApp extends StatefulWidget {
+  const AdminScreenApp({super.key, required this.title});
+  final String title;
+  @override
+  State<AdminScreenApp> createState() => _AdminScreenAppState();
+}
+
+class _AdminScreenAppState extends State<AdminScreenApp> {
+  static const _items = [
+      const MenuItem("ユーザー登録", Icons.person_add , Colors.blue,   WipScreen()),
+      const MenuItem("リソース登録", Icons.add_box,     Colors.red,    WipScreen()),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: MainDrawer(context),
+      appBar: MainAppBar(),
+      body: GridView.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0,
+        childAspectRatio: 1.77,
+        shrinkWrap: true,
+        children: List.generate(_items.length, (index) {
+          return ButtonTheme(
+            child: MaterialButton(
+              onPressed: () => {Navigator.push(context, MaterialPageRoute(builder: (context) => _items[index].route))},
+              color: _items[index].color,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(_items[index].icon, size: 28.0),
+                  Text(_items[index].name, style: TextStyle( fontSize: 28.0)),
+                ],
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
